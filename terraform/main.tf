c912b39e3f34335470598cb92c585fe54a5c529b
Kateryna Steshenko <98778431+kateryna666@users.noreply.github.com>
2026-05-08 21:42:48 +0300

    tf fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,8 +20,10 @@ locals {
     "run.googleapis.com",
     "cloudbuild.googleapis.com",
     "artifactregistry.googleapis.com",
+    "secretmanager.googleapis.com",
   ]
-  cloudbuild_sa = "${data.google_project.current.number}@cloudbuild.gserviceaccount.com"
+  cloudbuild_sa     = "${data.google_project.current.number}@cloudbuild.gserviceaccount.com"
+  cloudbuild_agent  = "service-${data.google_project.current.number}@gcp-sa-cloudbuild.iam.gserviceaccount.com"
 }
 
 resource "google_project_service" "apis" {
@@ -50,14 +52,56 @@ resource "google_project_iam_member" "cloudbuild_sa_user" {
   member  = "serviceAccount:${local.cloudbuild_sa}"
 }
 
-resource "google_cloudbuild_trigger" "deploy" {
-  name        = "${var.service_name}-deploy"
-  description = "Build and deploy to Cloud Run service ${var.service_name} on push to \"^main$\""
-  filename    = "cloudbuild.yaml"
+resource "google_secret_manager_secret" "github_pat" {
+  secret_id = "github-pat-cloudbuild"
+  replication {
+    auto {}
+  }
+  depends_on = [google_project_service.apis]
+}
 
-  github {
-    owner = var.github_owner
-    name  = var.github_repo
+resource "google_secret_manager_secret_version" "github_pat" {
+  secret      = google_secret_manager_secret.github_pat.id
+  secret_data = var.github_pat
+}
+
+resource "google_secret_manager_secret_iam_member" "cloudbuild_pat_access" {
+  secret_id = google_secret_manager_secret.github_pat.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${local.cloudbuild_agent}"
+}
+
+resource "google_cloudbuildv2_connection" "github" {
+  location = var.region
+  name     = "github-connection"
+
+  github_config {
+    app_installation_id = var.github_app_installation_id
+    authorizer_credential {
+      oauth_token_secret_version = google_secret_manager_secret_version.github_pat.id
+    }
+  }
+
+  depends_on = [
+    google_secret_manager_secret_iam_member.cloudbuild_pat_access,
+  ]
+}
+
+resource "google_cloudbuildv2_repository" "repo" {
+  location          = var.region
+  name              = var.github_repo
+  parent_connection = google_cloudbuildv2_connection.github.name
+  remote_uri        = "https://github.com/${var.github_owner}/${var.github_repo}.git"
+}
+
+
+resource "google_cloudbuild_trigger" "deploy" {
+  name     = "${var.service_name}-deploy"
+  location = var.region
+  filename = "cloudbuild.yaml"
+
+  repository_event_config {
+    repository = google_cloudbuildv2_repository.repo.id
     push {
       branch = "^main$"
     }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,3 +27,14 @@ variable "github_repo" {
   type        = string
   description = "GitHub repository name (without owner)."
 }
+
+variable "github_pat" {
+  type        = string
+  description = "GitHub Personal Access Token (classic, scope=repo) used by the Cloud Build v2 connection."
+  sensitive   = true
+}
+
+variable "github_app_installation_id" {
+  type        = string
+  description = "Installation ID of the Cloud Build GitHub App on your account. Find it at https://github.com/settings/installations → Configure → URL number."
+}
